31843335118852f237e862fe2814573a7e62945c
Farhan Elias <faarhaan10@gmail.com>
2021-11-29 12:23:12 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./Componants/Navigation/Navigation";
 import './style.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Componants/Home/Home/Home";
+import Footer from "./Componants/Footer/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/resume" component={Resume} /> */}
       </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ src/Componants/Footer/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+    AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+const Footer = () => {
+    let date = new Date();
+    let year = date.getFullYear();
+    return (
+        <Container fluid className="footer">
+            <Row>
+                <Col md="4" className="footer-copywright">
+                    <h3>Designed and Developed by Farhan Elias</h3>
+                </Col>
+                <Col md="4" className="footer-copywright">
+                    <h3>Copyright © {year} FARHAN</h3>
+                </Col>
+                <Col md="4" className="footer-body">
+                    <ul className="footer-icons">
+                        <li className="social-icons">
+                            <a
+                                href="https://github.com/faarhaan10"
+                                style={{ color: "white" }}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <AiFillGithub />
+                            </a>
+                        </li>
+                        <li className="social-icons">
+                            <a
+                                href="https://twitter.com/Soumyajit4419"
+                                style={{ color: "white" }}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <AiOutlineTwitter />
+                            </a>
+                        </li>
+                        <li className="social-icons">
+                            <a
+                                href="https://www.linkedin.com/in/farhan-elias/"
+                                style={{ color: "white" }}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaLinkedinIn />
+                            </a>
+                        </li>
+                        <li className="social-icons">
+                            <a
+                                href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
+                                style={{ color: "white" }}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <AiFillInstagram />
+                            </a>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Footer;
